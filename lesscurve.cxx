3c168f6ba137126baf4a78257c4af407f66bf319
scottsha <scott107@candytuft.llnl.gov>
2017-06-23 18:56:33 -0700

    new simple example

--- lesscurve.cxx
+++ lesscurve.cxx
@@ -38,101 +38,140 @@ vtkVector3d operator*(double scalar, vtkVector3d v){
   }
   return w;
 };
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
 
 vtkSmartPointer<vtkPolyData> build_small_simplicial_example()
 {
-  //Choose some points in parametric space
-  // double parametric_pts[6][2] = {
-  //   {0.0,0.0},
-  //   {0.0,1.0},
-  //   {0.0,2.0},
-  //   {1.0,0.0},
-  //   {1.0,1.0},
-  //   {2.0,0.0}
-  // };
-  // //Apply a ellipsoid parametrization to obtain points in R3
-  // int num_pts = sizeof(parametric_pts)/sizeof(parametric_pts[0]);
-  // auto points = vtkSmartPointer<vtkPoints>::New();
-  // points->SetNumberOfPoints(num_pts);
-  // for (int foo = 0; foo<num_pts; foo++){
-  //   double pt[3];
-  //   double p1 = .1 + .1 * parametric_pts[foo][0];
-  //   double p2 = .2 + .2 * parametric_pts[foo][1];
-  //   pt[0] = 5*cos(p1)*cos(p2);
-  //   pt[1] = 4*sin(p1)*cos(p2);
-  //   pt[2] = 3*sin(p2);
-  //   points->SetPoint(foo, pt[0], pt[1], pt[2]);
-  //   //std::cout << pt[0] << pt[1] << pt[2] << std::endl;
-  // }
-  // int cellList[4][3] = {
-  //   {0,1,3},
-  //   {1,3,4},
-  //   {1,2,4},
-  //   {3,4,5}
-  // };
-  // auto triangles = vtkSmartPointer<vtkCellArray>::New();
-  // for(int foo = 0; foo<sizeof(cellList)/sizeof(cellList[0]); foo++){
-  //   triangles->InsertNextCell(3);
-  //   for(int bar = 0; bar<sizeof(cellList[0])/sizeof(cellList[0][0]); bar++){
-  //     triangles->InsertCellPoint(cellList[foo][bar]);
-  //   }
-  // }
-
-  int xtris = 100;
-  int ytris = 100;
-  double param_pts[xtris*(ytris+1)][2];
-  int const num_tris = 2 * xtris * ytris;
-  int cell_list[num_tris][3];
-  for(int foo = 0 ; foo < num_tris; foo++){
-    int const sq = floor(foo/2);
-    int const is_down = foo%2;
-    int const pt_col = sq % xtris;
-    int const pt_row = floor(sq / xtris);
-    if(not is_down){
-      cell_list[foo][0]= pt_col  + xtris * pt_row;
-      cell_list[foo][1]=(1 + pt_col) % xtris  + xtris * (pt_row);
-      cell_list[foo][2]=0 + pt_col + xtris * (pt_row+1);
-    }
-    else{
-      cell_list[foo][0]=(1 + pt_col) % xtris + xtris * pt_row;
-      cell_list[foo][1]=(1 + pt_col) % xtris + xtris * (pt_row+1);
-      cell_list[foo][2]=0 + pt_col  + xtris * (pt_row+1);
-    }
-    //std::cout<<foo<<"cell "<<cell_list[foo][0]<<" "<<cell_list[foo][1]<<" "<<cell_list[foo][2]<<std::endl;
-    //std::copy( std::begin(pts_this_tri), std::end(pts_this_tri), cell_list[foo]);
-  }
-  double const y_shift=sqrt(3)/2;
-  double const x_shift=1;
-  for(int foo =0; foo < (xtris)*(ytris+1); foo++){
-    int pt_col = foo%xtris;
-    int pt_row = foo/xtris;
-    param_pts[foo][0]=pt_col + 0.5*pt_row;
-    param_pts[foo][1]=y_shift*pt_row;
-    //std::cout<<pt_col<<pt_row<<" "<<param_pts[foo][0]<<" "<<param_pts[foo][1]<<std::endl;
-  }
-  //Apply a ellipsoid parametrization to obtain points in R3
-  int num_pts = sizeof(param_pts)/sizeof(param_pts[0]);
+  std::string example_name ("simple2"); //"cylinder"
   auto points = vtkSmartPointer<vtkPoints>::New();
-  points->SetNumberOfPoints(num_pts);
-  for (int foo = 0; foo<num_pts; foo++){
-    double pt[3];
-    double p1 = param_pts[foo][0];
-    double p2 = param_pts[foo][1];
-    pt[0] = cos(2*M_PI*p1/xtris);
-    pt[1] = sin(2*M_PI*p1/xtris);
-    pt[2] = 2*M_PI*p2/ytris;
-    points->SetPoint(foo, pt[0], pt[1], pt[2]);
-    //std::cout << pt[0] << pt[1] << pt[2] << std::endl;
-  }
   auto triangles = vtkSmartPointer<vtkCellArray>::New();
-  for(int foo = 0; foo<sizeof(cell_list)/sizeof(cell_list[0]); foo++){
-    triangles->InsertNextCell(3);
-    //std::cout<<std::endl<<"Build cell" <<foo;
-    for(int bar = 0; bar<sizeof(cell_list[0])/sizeof(cell_list[0][0]); bar++){
-      triangles->InsertCellPoint(cell_list[foo][bar]);
-      //std::cout<<"point "<< cell_list[foo][bar];
+
+  if (example_name.compare("simple") ==0 ){
+    //Choose some points in parametric space
+    double parametric_pts[6][2] = {
+      {0.0,0.0},
+      {0.0,1.0},
+      {0.0,2.0},
+      {1.0,0.0},
+      {1.0,1.0},
+      {2.0,0.0}
+    };
+    //Apply a ellipsoid parametrization to obtain points in R3
+    int num_pts = sizeof(parametric_pts)/sizeof(parametric_pts[0]);
+    points->SetNumberOfPoints(num_pts);
+    for (int foo = 0; foo<num_pts; foo++){
+      double pt[3];
+      double p1 = .1 + .1 * parametric_pts[foo][0];
+      double p2 = .2 + .2 * parametric_pts[foo][1];
+      pt[0] = 5*cos(p1)*cos(p2);
+      pt[1] = 4*sin(p1)*cos(p2);
+      pt[2] = 3*sin(p2);
+      points->SetPoint(foo, pt[0], pt[1], pt[2]);
+      //std::cout << pt[0] << pt[1] << pt[2] << std::endl;
+    }
+    int cellList[4][3] = {
+      {0,1,3},
+      {1,3,4},
+      {1,2,4},
+      {3,4,5}
+    };
+    for(int foo = 0; foo<sizeof(cellList)/sizeof(cellList[0]); foo++){
+      triangles->InsertNextCell(3);
+      for(int bar = 0; bar<sizeof(cellList[0])/sizeof(cellList[0][0]); bar++){
+        triangles->InsertCellPoint(cellList[foo][bar]);
+      }
     }
   }
+
+  if (example_name.compare("simple2") == 0)
+  {
+    double angle0 = M_PI/6, angle1 = 0.0 , angle2 = 0.0 ;
+    double pt_pos[6][3] = {
+      {0,0,0},
+      {1,0,0},
+      {0,1,0},
+      {0,-1,tan(angle0)},
+      {-1,0,tan(angle1)},
+      {1,1,tan(angle2)/sqrt(2)}
+    };
+    int num_pts = sizeof(pt_pos)/sizeof(pt_pos[0]);
+    points->SetNumberOfPoints(num_pts);
+    for (vtkIdType foo = 0; foo < num_pts; foo++) {
+      double p0 = pt_pos[foo][0], p1 = pt_pos[foo][1], p2 = pt_pos[foo][2];
+      points->SetPoint(foo, p0, p1, p2);
+      // std::cout << points->GetNumberOfPoints() << " " << pt_pos[foo][0] << pt_pos[foo][1] << pt_pos[foo][2] << std::endl;
+    }
+    int cellList[4][3] = {
+      {0,1,2},
+      {0,3,1},
+      {0,2,4},
+      {1,5,2}
+    };
+    for(int foo = 0; foo<sizeof(cellList)/sizeof(cellList[0]); foo++){
+      triangles->InsertNextCell(sizeof(cellList[0])/sizeof(cellList[0][0]));
+      for(int bar = 0; bar<sizeof(cellList[0])/sizeof(cellList[0][0]); bar++){
+        triangles->InsertCellPoint(cellList[foo][bar]);
+      }
+    }
+  }
+  if (example_name.compare("cylinder") == 0)
+  {
+    int xtris = 70;
+    int ytris = 70;
+    double param_pts[xtris*(ytris+1)][2];
+    int const num_tris = 2 * xtris * ytris;
+    int cell_list[num_tris][3];
+    for(int foo = 0 ; foo < num_tris; foo++){
+      int const sq = floor(foo/2);
+      int const is_down = foo%2;
+      int const pt_col = sq % xtris;
+      int const pt_row = floor(sq / xtris);
+      if(not is_down){
+        cell_list[foo][0]= pt_col  + xtris * pt_row;
+        cell_list[foo][1]=(1 + pt_col) % xtris  + xtris * (pt_row);
+        cell_list[foo][2]=0 + pt_col + xtris * (pt_row+1);
+      }
+      else{
+        cell_list[foo][0]=(1 + pt_col) % xtris + xtris * pt_row;
+        cell_list[foo][1]=(1 + pt_col) % xtris + xtris * (pt_row+1);
+        cell_list[foo][2]=0 + pt_col  + xtris * (pt_row+1);
+      }
+    }
+    double const y_shift=sqrt(3)/2;
+    double const x_shift=1;
+    for(int foo =0; foo < (xtris)*(ytris+1); foo++){
+      int pt_col = foo%xtris;
+      int pt_row = foo/xtris;
+      param_pts[foo][0]=pt_col + 0.5*pt_row;
+      param_pts[foo][1]=y_shift*pt_row;
+      //std::cout<<pt_col<<pt_row<<" "<<param_pts[foo][0]<<" "<<param_pts[foo][1]<<std::endl;
+    }
+    //Apply a ellipsoid parametrization to obtain points in R3
+    int num_pts = sizeof(param_pts)/sizeof(param_pts[0]);
+    points->SetNumberOfPoints(num_pts);
+    for (int foo = 0; foo<num_pts; foo++){
+      double pt[3];
+      double p1 = param_pts[foo][0];
+      double p2 = param_pts[foo][1];
+      pt[0] = cos(2*M_PI*p1/xtris);
+      pt[1] = sin(2*M_PI*p1/xtris);
+      pt[2] = 2*M_PI*p2/ytris;
+      points->SetPoint(foo, pt[0], pt[1], pt[2]);
+      //std::cout << pt[0] << pt[1] << pt[2] << std::endl;
+    }
+    for(int foo = 0; foo<sizeof(cell_list)/sizeof(cell_list[0]); foo++){
+      triangles->InsertNextCell(3);
+      //std::cout<<std::endl<<"Build cell" <<foo;
+      for(int bar = 0; bar<sizeof(cell_list[0])/sizeof(cell_list[0][0]); bar++){
+        triangles->InsertCellPoint(cell_list[foo][bar]);
+        //std::cout<<"point "<< cell_list[foo][bar];
+      }
+    }
+  }
+
   auto simplicial_complex = vtkSmartPointer<vtkPolyData>::New();
   simplicial_complex->SetPoints(points);
   simplicial_complex->SetPolys(triangles);
@@ -190,17 +229,18 @@ vtkSmartPointer<vtkPolyData> compute_minor_curvature_field(vtkSmartPointer<vtkPo
     // The shape operator is symmetric and computed in local edge coordinates.
     // Note that since we only care about direction, the shape operator is not
     // locally normalized so that the curvature is incorrect.
-    double edge_lengths[3], shape00, shape01, shape11, angle, edott0=0, edott1=0;
-    vtkVector3d edges[3], tang_vects[3];
+    double edge_lengths[3], shape00, shape01, shape11;
+    vtkVector3d edges[3];// tang_vects[3];
     int edge_ends[3][3] = {{2,1},{0,2},{1,0}};
+    if(foo==0)std::cout<<"Normal "<< normal[0] << normal[1] << normal[2]<<std::endl;
     //Iterating over the edges of the triangle
+    double angles[3];
     for(int bar=0; bar<3; bar ++){
       //Edge vectors for orientation 012
       edges[bar] = pts [ edge_ends[bar][0] ] - pts [ edge_ends[bar][1] ];
       //Edge lengths
       edge_lengths[bar]=edges[bar].Norm();
-      //Normalize edges
-      edges[bar].Normalize();
+      if(foo==0)std::cout<< edges[bar][0] << edges[bar][1] << edges[bar][2]<<std::endl;
       //Find neighboring cell across edge bar
       auto cell_ngb = vtkSmartPointer<vtkIdList>::New();
       surface->GetCellEdgeNeighbors(foo,
@@ -213,17 +253,21 @@ vtkSmartPointer<vtkPolyData> compute_minor_curvature_field(vtkSmartPointer<vtkPo
       if(cell_ngb->GetNumberOfIds()>0){
         normals->GetTuple(cell_ngb->GetId(0),temp_n);
         vtkVector3d ngb_normal(temp_n);
+        if(foo==0)std::cout<<"That normal "<< ngb_normal[0]  <<" "<< ngb_normal[1] << " " << ngb_normal[2]<<std::endl;
         //The sign of the angle is taken to agree with right-hand rotation
         //about the edge vector.
-        angle = asin( edges[bar].Dot( normal.Cross(ngb_normal) ) ) * edge_lengths[bar] ;
+        angles[bar] = asin( edges[bar].Dot( normal.Cross(ngb_normal) ) / edge_lengths[bar] ) ;
+        angles[bar] = angles[bar]/edge_lengths[bar];
+        if(foo==0)std::cout<<"angle "<< angles[bar]<<std::endl;
         //This is computed over the basis edge[0]=p2-p1, edge[1]=p0-p2
-        edott0=edges[0].Dot(edges[bar]);
-        edott1=edges[1].Dot(edges[bar]);
-        //Compute the shape operator matrix entries shape10=shape01 except the eigenvalues are reversed
-        shape00+=angle * edott0 * edott0;
-        shape01+=angle * edott0 * edott1;
-        shape11+=angle * edott1 * edott1;
+        //Compute the shape operator matrix entries shape10=shape01
+        //but except the eigenvalues are associated with the opposite eigenvectors
+        //as in ~H of https://graphics.stanford.edu/courses/cs468-03-fall/Papers/cohen_normalcycle.pdf
       }
+      shape00 = angles[0]+angles[2];
+      shape01 = angles[2];
+      shape11 = angles[1]+angles[2];
+      if(foo==0)std::cout<<"shape "<< shape00<<" "<< shape01<< " " <<shape11 <<std::endl;
     }
   //Compute the minor eigenvalue and the corresponding eigenvector
   double shape_trace=shape00+shape11;
@@ -235,6 +279,7 @@ vtkSmartPointer<vtkPolyData> compute_minor_curvature_field(vtkSmartPointer<vtkPo
   else{
     shape_small_eig=(shape_trace - sqrt(shape_discr))/2.0;
   }
+  if(foo==0) std::cout<< "Middle eigen " <<shape_small_eig <<std::endl;
   double minor_curv_vect[2] = { shape01 , shape_small_eig-shape00 };
   //Should this get normalized? There's currently nothing to stop crashing
   //if the curvature is uniformly 0
@@ -249,6 +294,41 @@ vtkSmartPointer<vtkPolyData> compute_minor_curvature_field(vtkSmartPointer<vtkPo
   double * w = &euc_curv_vect[0];
   euc_minor_curv_field->SetTuple( foo , w);
   }
+  //Looks noisey. Try a uniform window blur on cell neighbors.
+  //There should be a fast way to do this with a convolution filter....
+
+  const char* blur_name="BlurredField";
+  auto blurred_field = vtkSmartPointer<vtkFloatArray>::New();
+  blurred_field->SetNumberOfComponents(3);
+  blurred_field->SetNumberOfTuples(surface->GetNumberOfCells());
+  blurred_field->SetName(blur_name);
+  bool apply_blur = false;
+  if(apply_blur){
+    for(int foo = 0; foo<surface->GetNumberOfCells(); foo++){
+      double tempf[3];
+      euc_minor_curv_field->GetTuple( foo , tempf);
+      vtkVector3d field_here(tempf);
+      int edge_ends[3][3] = {{2,1},{0,2},{1,0}};
+      auto cell_pts = vtkSmartPointer<vtkIdList>::New();
+      surface->GetCellPoints(foo,cell_pts);
+      auto cell_ngb = vtkSmartPointer<vtkIdList>::New();
+      for(int bar=0;bar<3;bar++){
+        surface->GetCellEdgeNeighbors(foo,
+        cell_pts->GetId(edge_ends[bar][0]),
+        cell_pts->GetId(edge_ends[bar][0]),
+        cell_ngb);
+        if(cell_ngb->GetNumberOfIds()>0){
+          euc_minor_curv_field->GetTuple(cell_ngb->GetId(0), tempf);
+          vtkVector3d field_there(tempf);
+          field_here=field_here+field_there;
+        }
+      }
+      field_here.Normalize();
+      double * w = &field_here[0];
+      blurred_field->SetTuple(foo, w);
+    }
+  }
+
   // If an old curvature array already exists we remove it
   if (surface->GetCellData()->HasArray(minor_name))
     surface->GetCellData()->RemoveArray(minor_name);
@@ -258,6 +338,13 @@ vtkSmartPointer<vtkPolyData> compute_minor_curvature_field(vtkSmartPointer<vtkPo
 
   surface->GetCellData()->AddArray(minor_curv_field);
   surface->GetCellData()->AddArray(euc_minor_curv_field);
+
+  if(apply_blur){
+    if (surface->GetCellData()->HasArray(blur_name))
+      surface->GetCellData()->RemoveArray(blur_name);
+    surface->GetCellData()->AddArray(blurred_field);
+  }
+
   return surface;
 }
 
@@ -314,11 +401,9 @@ int main(int argc, const char* argv[])
   }
   else{
     output_name = "bongo.vtp";
-    {
-      int g;
+    int g;
     surface = build_small_simplicial_example();
-    //std::cout<<"After build cells numbering "<<surface->GetNumberOfCells() << std::endl;
-  }
+    // std::cout<<"After build cells numbering "<<surface->GetNumberOfCells() << std::endl;
   }
 
   // auto surface = vtkSmartPointer<vtkPolyData>::New();
